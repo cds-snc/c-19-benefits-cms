497f556ff8600925f9ab64fe2613794d3eba43ef
Kate Drwecka <katedrwecka@gmail.com>
2020-03-25 14:36:23 -0400

    remove auto css fetch on admin as it wasn't working very well

--- admin/preview-templates/index.js
+++ admin/preview-templates/index.js
@@ -4,16 +4,3 @@ CMS.registerPreviewTemplate("enPages", Page);
 CMS.registerPreviewTemplate("frPages", Page);
 
 CMS.registerPreviewStyle("/static/assets/GCWeb/css/theme.min.css");
-
-// Register any CSS file on the home page as a preview style
-fetch("/en/")
-  .then(response => response.text())
-  .then(html => {
-    const f = document.createElement("html");
-    f.innerHTML = html;
-    Array.from(f.getElementsByTagName("link")).forEach(tag => {
-      if (tag.rel == "stylesheet" && !tag.media) {
-        CMS.registerPreviewStyle(tag.href);
-      }
-    });
-  });
